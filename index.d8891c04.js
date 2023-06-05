8614dda702b5f60a5aab9013d7f42de64713194e
MarcinKowalczykk <124718110+MarcinKowalczykk@users.noreply.github.com>
2023-06-05 17:04:31 +0000

    Deploying to gh-pages from @ MarcinKowalczykk/Anemone@90407ffd826d8de8617e04e8e4f58c8eba8d1bcd 🚀

---
+++ index.d8891c04.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".slide-content",{slidesPerView:3,spaceBetween:25,loop:!0,centerSlide:"true",fade:"true",grabCursor:"true",pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{0:{slidesPerView:1},520:{slidesPerView:2},950:{slidesPerView:3}}});
+//# sourceMappingURL=index.d8891c04.js.map
