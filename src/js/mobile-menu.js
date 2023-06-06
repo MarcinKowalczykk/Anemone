5fb5574313ddbbdab8b26875b2daf756da52c9ab
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-06-06 20:47:17 +0200

    Squashed commit of the following:
    commit 267434d6a5650bf27289c4427f5ddde64dfb60cf
    Merge: 98a04fc 59cadf7
    Author: Marcin Kowalczyk <124718110+MarcinKowalczykk@users.noreply.github.com>
    Date:   Tue Jun 6 20:38:41 2023 +0200
    
        Merge pull request #25 from MarcinKowalczykk/Natalia-Nowak/line-above-e
    
        Added the line above the hero header
    
    commit 98a04fc8d0c7c016e6985bd09f29fdcf1dc257d6
    Author: Marcin Kowalczyk <marcinck80@gmail.com>
    Date:   Tue Jun 6 20:33:46 2023 +0200
    
        fixed photos
    
    commit 59cadf7f35d843f0f6a6dba3b190f4aeb2e80230
    Author: Natalia-Nowak <nowak.natalia.080@gmail.com>
    Date:   Tue Jun 6 20:14:30 2023 +0200
    
        Added the line above the hero header
    
    commit 74f80021034fa6df328656151e60e0eb19568b6c
    Merge: 553ed88 cf8e6c4
    Author: Marcin Kowalczyk <124718110+MarcinKowalczykk@users.noreply.github.com>
    Date:   Tue Jun 6 20:04:56 2023 +0200
    
        Merge pull request #23 from MarcinKowalczykk/Mariusz-Guzowski/modal-contactu-us
    
        mobile-menu
    
    commit cf8e6c41bbb2f81023fd1e26caa59dbb18ac849d
    Author: Gugiet <mguzowski92@gmail.com>
    Date:   Tue Jun 6 20:01:46 2023 +0200
    
        mobile-menu
    
    commit 553ed883350e1ac12677df6bd068988bbfc51ba0
    Author: Marcin Kowalczyk <marcinck80@gmail.com>
    Date:   Tue Jun 6 20:01:03 2023 +0200
    
        Update _about.scss
    
    commit ab2d4ebe7b01bcbc959167c1c37cc0ea8317bee0
    Author: Marcin Kowalczyk <marcinck80@gmail.com>
    Date:   Tue Jun 6 19:35:46 2023 +0200
    
        bugs fixing
    
    commit b9cb05fa662c8ab196eebacdf9736fd6cc48c446
    Author: Marcin Kowalczyk <marcinck80@gmail.com>
    Date:   Tue Jun 6 18:42:57 2023 +0200
    
        wszystkiego po trochu fixes

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const menuBtn = document.querySelector('.js-open-menu');
   const menuEl = document.querySelectorAll('.mobile-menu__link');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
     const isMenuOpen = menuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -14,7 +15,10 @@
 
   menuBtn.addEventListener('click', toggleMenu);
   menuEl.forEach(link => link.addEventListener('click', toggleMenu));
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
